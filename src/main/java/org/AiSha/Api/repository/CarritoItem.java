d97fcd06516bba36ecae368d7a564c739a24c04a
OdraudeMendezAguirre <mendez.odraude.1dm@gmail.com>
2022-05-23 19:09:32 -0500

    Carrito

--- src/main/java/org/AiSha/Api/repository/CarritoItem.java
+++ src/main/java/org/AiSha/Api/repository/CarritoItem.java
@@ -41,11 +41,11 @@ public class CarritoItem {
         this.nombre = nombre;
     }
 
-    public String getPrecio() {
+    public int getPrecio() {
         return precio;
     }
 
-    public void setPrecio(String precio) {
+    public void setPrecio(int precio) {
         this.precio = precio;
     }
     
